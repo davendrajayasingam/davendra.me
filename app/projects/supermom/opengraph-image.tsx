156a8996946ccb836c5915782dd41283975bd95d
Davendra Jayasingam <hello@websplorer.com>
2023-06-20 03:02:05 +0800

    Updated OG Images with custom font

--- app/projects/supermom/opengraph-image.tsx
+++ app/projects/supermom/opengraph-image.tsx
@@ -5,7 +5,7 @@ import coverImage from '@/app/projects/supermom/images/supermom-cover.png'
 
 export const runtime = 'edge'
 
-export const alt = 'SUPERMOM PROJECT'
+export const alt = 'SUPERMOM'
 export const size = {
     width: 1200,
     height: 630
@@ -13,28 +13,48 @@ export const size = {
 
 export const contentType = 'image/png'
 
+const righteousFont = fetch(
+    new URL('/assets/Righteous-Regular.ttf', import.meta.url)
+).then((res) => res.arrayBuffer())
+
 export default async function Image() 
 {
     return new ImageResponse(
         (
-            <div
-                tw='flex flex-col justify-center items-center w-full h-full bg-cover bg-center bg-no-repeat'
-                style={{
-                    backgroundImage: `url(https://davendra.me${coverImage.src})`
-                }}
-            >
-                <div tw='flex flex-col items-center justify-center w-full h-full bg-black bg-opacity-30'>
+            <div tw='flex flex-col items-center justify-center w-full h-full bg-teal-600'>
+                <img
+                    src={`https://davendra.me${coverImage.src}`}
+                    alt='Cover'
+                    width={1200}
+                />
+
+                <div tw='flex absolute left-0 right-0 bottom-0 h-[130px] bg-teal-600 flex items-center justify-center'>
                     <img
                         src='https://davendra.me/images/logo-teal.png'
                         alt='Davendra Jayasingam'
-                        width={1000}
+                        height={100}
                     />
-
-                    <p tw='flex font-bold bg-teal-100 rounded-full px-12 py-3 text-teal-600 text-6xl'>
-                        {`https://davendra.me${coverImage.src}`}
+                    <p
+                        tw='font-bold text-teal-100 text-6xl ml-4'
+                        style={{
+                            fontFamily: '"Righteous"'
+                        }}
+                    >
+                        {alt}
                     </p>
                 </div>
             </div>
-        )
+        ),
+        {
+            ...size,
+            fonts: [
+                {
+                    name: 'Righteous',
+                    data: await righteousFont,
+                    style: 'normal',
+                    weight: 400
+                }
+            ]
+        }
     )
 }
--- app/projects/the-end/opengraph-image.tsx
+++ app/projects/the-end/opengraph-image.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from 'next/server'
 
+import coverImage from '@/app/projects/the-end/images/the-end-cover.png'
+
 export const runtime = 'edge'
 
-export const alt = 'The End Project'
+export const alt = 'The End'
 export const size = {
     width: 1200,
     height: 630
@@ -11,32 +13,48 @@ export const size = {
 
 export const contentType = 'image/png'
 
+const righteousFont = fetch(
+    new URL('/assets/Righteous-Regular.ttf', import.meta.url)
+).then((res) => res.arrayBuffer())
+
 export default async function Image() 
 {
     return new ImageResponse(
         (
-            <div
-                tw='flex flex-col justify-center items-center w-full h-full bg-cover bg-center bg-no-repeat'
-                style={{
-                    backgroundImage: 'url(https://davendra.me/images/og-bg.png)'
-                }}
-            >
-                <div tw='flex flex-col items-center justify-center w-full h-full bg-black bg-opacity-30'>
+            <div tw='flex flex-col items-center justify-center w-full h-full bg-teal-600'>
+                <img
+                    src={`https://davendra.me${coverImage.src}`}
+                    alt='Cover'
+                    width={1200}
+                />
+
+                <div tw='flex absolute left-0 right-0 bottom-0 h-[130px] bg-teal-600 flex items-center justify-center'>
                     <img
                         src='https://davendra.me/images/logo-teal.png'
                         alt='Davendra Jayasingam'
-                        width={1000}
+                        height={100}
                     />
-
-                    <p tw='flex font-bold text-teal-100 text-6xl'>
-                        FULL STACK WEB DEVELOPER
-                    </p>
-
-                    <p tw='flex font-bold bg-teal-100 rounded-full px-12 py-3 text-teal-600 text-6xl uppercase'>
+                    <p
+                        tw='font-bold text-teal-100 text-6xl ml-4'
+                        style={{
+                            fontFamily: '"Righteous"'
+                        }}
+                    >
                         {alt}
                     </p>
                 </div>
             </div>
-        )
+        ),
+        {
+            ...size,
+            fonts: [
+                {
+                    name: 'Righteous',
+                    data: await righteousFont,
+                    style: 'normal',
+                    weight: 400
+                }
+            ]
+        }
     )
 }
